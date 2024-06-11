77275ba0fb139471d30c96188fcad1987335d75f
Fokku <shodusbus@gmail.com>
2024-06-11 22:55:37 +0800

    Added analytics

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,23 @@
-import { TelegramGroups, columns } from "./columns"
-import { DataTable } from "./data-table"
-import master_list from "../data/master_list.json"
-import Navbar from "./components/Navbar"
+import { TelegramGroups, columns } from "./columns";
+import { DataTable } from "./data-table";
+import master_list from "../data/master_list.json";
+import Navbar from "./components/Navbar";
+import { Analytics } from "@vercel/analytics/react";
 
 function getData(): TelegramGroups[] {
-  return master_list
+  return master_list;
 }
 
 export default async function DemoPage() {
-  const data = await getData()
+  const data = await getData();
 
   return (
     <div>
-      <Navbar/>
-    <div className="container mx-auto py-10">
-      <DataTable columns={columns} data={data} />
+      <Navbar />
+      <div className="container mx-auto py-10">
+        <DataTable columns={columns} data={data} />
+        <Analytics />
+      </div>
     </div>
-    </div>
-  )
+  );
 }
